9d01227601ce0951bb90f711fbae3a6ef47c1733
J. Salvador Arias <jsalarias@gmail.com>
2025-10-07 12:17:47 +0300

    matrix: equate model parameters

--- R/matrix.r
+++ R/matrix.r
@@ -143,6 +143,40 @@ model_matrix <- function(model = "") {
   ), nrow = 4, byrow = TRUE))
 }
 
+#' @export
+#' @title Make Equivalent Two or More Parameters
+#'
+#' @description
+#' `model_equate()` read a model definition
+#' and make the indicated parameters
+#' to be identical.
+#'
+#' @param model A model definition as build with `new_model()`.
+#' @param params A vector with the parameter IDs to be equated.
+model_equate <- function(model, params) {
+  if (!inherits(model, "sinba_model")) {
+    stop("model_equate: `model` must be an object of class \"sinba_model\".")
+  }
+  m <- model$model
+  if (length(params) < 2) {
+    stop("model_equate: `params` should at least have two elements.")
+  }
+
+  p <- params[1]
+  for (cc in seq_len(ncol(m))) {
+    for (r in seq_len(nrow(m))) {
+      if (m[r, cc] %in% params) {
+        m[r, cc] <- p
+      }
+    }
+  }
+
+  m <- format_model_matrix(m)
+  model$name <- "user defined"
+  model$model <- m
+  return(model)
+}
+
 # from_model_to_Q sets a Q matrix
 # from a model matrix
 # and a set of parameter values.
